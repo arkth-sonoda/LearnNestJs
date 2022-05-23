07accd3424638cc5bce705ba0e7f8325ffb2e25d
Junya Sonoda <j.sonoda@arkth.co.jp>
2022-05-23 11:44:45 +0900

    クラスバリデーション作成

--- sample-app/src/users/dto/create-user.dto.ts
+++ sample-app/src/users/dto/create-user.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(10)
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(10)
   password: string;
 }
